97fde16e2bf458e8cc4236d8e9106e48df10e5b5
Aadharshramanathan <87597513+Aadharshramanathan@users.noreply.github.com>
2022-09-18 13:17:33 +0530

    Add files via upload

---
+++ Assignments/Aadharsh R/Assignment-1/1.py
@@ -0,0 +1,21 @@
+#Creates a list
+list1 = [1,2,3,4]
+
+#insert int at a position
+list1[0] = 0
+
+#Delete the first occurance
+list1.pop(0)
+
+#Insert at the end
+list1.append(5)
+print(list1)
+
+#Sort the list
+list1.sort()
+print(list1)
+
+#pop the last element
+list1.pop()
+
+print(list1)
---
+++ Assignments/Aadharsh R/Assignment-1/3.py
@@ -0,0 +1,14 @@
+# Defining strings
+var1 = "IBM "
+var2 = " CLOUD"
+
+# + Operator is used to concatenate strings
+var3 = var1 + var2
+print(var3)
+
+# String slicing
+String = 'INVENTORY MANAGEMENT SYSTEM FOR RETAILER'
+
+# Using indexing sequence
+print(String[:2])
+print(String[0:4:2])
